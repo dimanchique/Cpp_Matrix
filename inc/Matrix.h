a79a1a634d9ab9501e05727a90bb39c27b380760
ghettorunner@yandex.ru <ghettorunner@yandex.ru>
2022-12-18 19:59:18 +0300

    Proxy vector and Timer moved to Matrix.h

--- inc/Matrix.h
+++ inc/Matrix.h
@@ -1,16 +1,100 @@
 #pragma once
 
-#include "ProxyVector.h"
 #include "vector"
 #include <cassert>
+#include "iostream"
 
 //#define DEBUG
 
-#ifndef DEBUG
-#include "iostream"
-#else
-#include "Timer.h"
+#ifdef DEBUG
+#include <chrono>
+#include <string>
+
+struct Timer{
+    std::chrono::high_resolution_clock::time_point start, end;
+    std::chrono::duration<float> duration{};
+    std::string function_name;
+
+
+    explicit Timer(const std::string &name)
+    {
+        start = std::chrono::high_resolution_clock::now();
+        function_name = name;
+    }
+
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        float ms = duration.count() * 1000.0f;
+        std::cout << "Execution of " << function_name.c_str() << " took " << ms << "ms\n";
+    }
+};
 #endif
+
+template<typename Type>
+class ProxyVector {
+private:
+    std::vector<Type> data{};
+
+public:
+    ProxyVector() = default;
+    explicit ProxyVector(const int Size) : data(Size) {}
+    explicit ProxyVector(std::vector<Type> &Data) : data(Data) {}
+    ~ProxyVector() = default;
+
+    [[nodiscard]] size_t size() const { return data.size(); }
+
+    void erase(int idx){
+        data.erase(data.begin() + idx);
+    }
+
+    ProxyVector &operator=(const std::vector<Type> &new_data){
+        data = new_data;
+        return *this;
+    }
+
+    bool operator==(const ProxyVector &InVector) const{
+        bool SizeEquality = size() == InVector.size();
+        bool ValuesEquality = true;
+        for (int i = 0; i < size(); i++)
+            if (data[i] != InVector[i]) {
+                ValuesEquality = false;
+                break;
+            }
+        return SizeEquality && ValuesEquality;
+    }
+
+    bool operator!=(const ProxyVector &InVector) const{
+        return *this != InVector;
+    }
+
+    bool operator==(const std::vector<Type> &InVector) const{
+        bool SizeEquality = size() == InVector.size();
+        bool ValuesEquality = true;
+        for (int i = 0; i < size(); i++)
+            if (data[i] != InVector[i]) {
+                ValuesEquality = false;
+                break;
+            }
+        return SizeEquality && ValuesEquality;
+    }
+
+    bool operator!=(const std::vector<Type> &InVector) const{
+        return *this != InVector;
+    }
+
+    Type &operator[](const int idx){
+        assert(idx < data.size());
+        return data[idx];
+    }
+
+    Type operator[](const int idx) const{
+        assert(idx < data.size());
+        return data[idx];
+    }
+};
+
 
 template<typename Type>
 class Matrix {
